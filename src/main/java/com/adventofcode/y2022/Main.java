c22d99659b08ecd9c84a1db64f58b5875a5d2e27
afcasco <7701272+afcasco@users.noreply.github.com>
2022-12-01 19:15:01 +0100

    naming

--- src/main/java/com/adventofcode/y2022/Main.java
+++ src/main/java/com/adventofcode/y2022/Main.java
@@ -7,6 +7,7 @@ import com.adventofcode.y2022.day01.Part2;
 public class Main {
     public static void main(String[] args) {
 
+        // Dang elf mate, you gonna eat that?
         System.out.println("Day 1 solution: " + Part1.solution() + " calories.");
         System.out.println("Day 2 solution: " + Part2.solution() + " calories.");
     }

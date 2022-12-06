f3a119add4e5b4ea37f97c000107f9c40b33d3a3
afcasco <7701272+afcasco@users.noreply.github.com>
2022-12-06 12:16:47 +0100

    parallelized the streams, it benefits greatly and it's up to 4 times faster

--- src/main/java/com/adventofcode/y2022/day06/Main.java
+++ src/main/java/com/adventofcode/y2022/day06/Main.java
@@ -1,10 +1,21 @@
 package com.adventofcode.y2022.day06;
 
+import com.adventofcode.utils.IOUtils;
+
 import java.io.IOException;
+import java.util.stream.IntStream;
 
 public class Main {
+
+    public static int findStart(String input, int length) throws IOException {
+        String data = IOUtils.readInputFile(input).get(0);
+        return IntStream.range(length, data.length()).parallel()
+                .filter(i -> data.substring(i - length, i).chars().distinct().count() == length)
+                .findFirst().orElse(-1);
+    }
+
     public static void main(String[] args) throws IOException {
-        System.out.println("Part 1 result: " + Part1.findStartOfPacket("day06input"));
-        System.out.println("Part 2 result: " + Part2.findStartOfMessage("day06input"));
+        System.out.println("Part 1 result: " + findStart("day06input", 4));
+        System.out.println("Part 2 result: " + findStart("day06input", 14));
     }
 }

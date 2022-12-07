dfaea7edb665ddb4dcb3551b0a4513ed6c283771
afcasco <7701272+afcasco@users.noreply.github.com>
2022-12-07 16:48:35 +0100

    day07 cleaned up  a bit

--- src/main/java/com/adventofcode/y2022/day07/FSElement.java
+++ src/main/java/com/adventofcode/y2022/day07/FSElement.java
@@ -7,14 +7,6 @@ public class FSElement {
     private final int size;
     private final Path path;
 
-    public long getSize() {
-        return size;
-    }
-
-    public Path getPath() {
-        return path;
-    }
-
     public FSElement(String fileName, Path path) {
         if (Character.isDigit(fileName.charAt(0))) {
             this.size = Integer.parseInt(fileName.split(" ")[0]);
@@ -22,5 +14,13 @@ public class FSElement {
             this.size = 0;
         }
         this.path = path;
+    }
+
+    public long getSize() {
+        return this.size;
+    }
+
+    public Path getPath() {
+        return this.path;
     }
 }

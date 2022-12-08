81ed8154fcc976523117542cbfc573fb5eef24f5
afcasco <7701272+afcasco@users.noreply.github.com>
2022-12-08 13:33:57 +0100

    completed day 08

---
+++ src/main/java/com/adventofcode/y2022/day08/Part1.java
@@ -0,0 +1,72 @@
+package com.adventofcode.y2022.day08;
+
+import com.adventofcode.utils.IOUtils;
+
+import java.io.IOException;
+import java.util.List;
+
+
+public class Part1 {
+
+    public static void main(String[] args) throws IOException {
+        List<String> data = IOUtils.readInputFile("day08input");
+        int visibleTrees = 0;
+        int[][] patch = new int[99][99];
+        for (int i = 0; i < 99; i++) {
+            for (int j = 0; j < 99; j++) {
+                patch[i][j] = data.get(i).charAt(j) - 48;
+            }
+        }
+
+        for (int i = 0; i < 99; i++) {
+            for (int j = 0; j < 99; j++) {
+                if (visibleFromLeft(i, j, patch) || visibleFromRight(i, j, patch)
+                        || visibleFromTop(i, j, patch) || visibleFromBottom(i, j, patch)) {
+                    visibleTrees++;
+                }
+            }
+        }
+
+        System.out.println("Elfvisibilitymeter counted " + visibleTrees + " visible trees on this patch");
+    }
+
+    static boolean visibleFromTop(int row, int col, int[][] patch) {
+        boolean clear = true;
+        int i = row - 1;
+        while (i >= 0 && clear) {
+            clear = patch[i][col] < patch[row][col];
+            i--;
+        }
+        return clear;
+    }
+
+    static boolean visibleFromBottom(int row, int col, int[][] patch) {
+        boolean clear = true;
+        int i = row + 1;
+        while (i < patch.length && clear) {
+            clear = patch[i][col] < patch[row][col];
+            i++;
+        }
+        return clear;
+    }
+
+    static boolean visibleFromLeft(int row, int col, int[][] patch) {
+        boolean clear = true;
+        int i = col - 1;
+        while (i >= 0 && clear) {
+            clear = patch[row][i] < patch[row][col];
+            i--;
+        }
+        return clear;
+    }
+
+    static boolean visibleFromRight(int row, int col, int[][] patch) {
+        boolean clear = true;
+        int i = col + 1;
+        while (i < patch.length && clear) {
+            clear = patch[row][i] < patch[row][col];
+            i++;
+        }
+        return clear;
+    }
+}
---
+++ src/main/java/com/adventofcode/y2022/day08/Part2.java
@@ -0,0 +1,83 @@
+package com.adventofcode.y2022.day08;
+
+import com.adventofcode.utils.IOUtils;
+
+import java.io.IOException;
+import java.util.List;
+
+// my answer 313200
+public class Part2 {
+
+    private static final int ROWS = 99;
+    private static final int COLS = 99;
+    private static final int[][] PATCH = new int[ROWS][COLS];
+
+    public static void main(String[] args) throws IOException {
+        List<String> data = IOUtils.readInputFile("day08input");
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                PATCH[i][j] = data.get(i).charAt(j) - 48;
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < PATCH.length; i++) {
+            for (int j = 0; j < PATCH.length; j++) {
+                int current = visibleFromTop(i, j) * visibleFromBottom(i, j)
+                        * visibleFromLeft(i, j) * visibleFromRight(i, j);
+                if (current > max) {
+                    max = current;
+                }
+            }
+        }
+        System.out.println("max: " + max);
+    }
+
+    static int visibleFromTop(int row, int col) {
+        boolean clear = true;
+        int range = 0;
+        int i = row - 1;
+        while (i >= 0 && clear) {
+            clear = PATCH[i][col] < PATCH[row][col];
+            range++;
+            i--;
+        }
+        return range;
+    }
+
+    static int visibleFromBottom(int row, int col) {
+        boolean clear = true;
+        int range = 0;
+        int i = row + 1;
+        while (i < PATCH.length && clear) {
+            clear = PATCH[i][col] < PATCH[row][col];
+            range++;
+            i++;
+        }
+        return range;
+    }
+
+    static int visibleFromLeft(int row, int col) {
+        boolean clear = true;
+        int range = 0;
+        int i = col - 1;
+        while (i >= 0 && clear) {
+            clear = PATCH[row][i] < PATCH[row][col];
+            range++;
+            i--;
+        }
+        return range;
+    }
+
+    static int visibleFromRight(int row, int col) {
+        boolean clear = true;
+        int range = 0;
+        int i = col + 1;
+        while (i < PATCH.length && clear) {
+            clear = PATCH[row][i] < PATCH[row][col];
+            range++;
+            i++;
+        }
+        return range;
+    }
+}
